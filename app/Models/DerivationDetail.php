959e121b28077f48311ab4a2aacbac944c9625ae
RobertoRuben <robertoch263@gmail.com>
2025-05-13 17:29:28 -0500

    ✨ feat(derivation-detail): Añadir campos user_id y status a la tabla derivation_details

--- app/Models/DerivationDetail.php
+++ app/Models/DerivationDetail.php
@@ -10,11 +10,18 @@ class DerivationDetail extends Model
     protected $table = 'derivation_details';
     protected $fillable = [
         'derivation_id',
-        'comments'
+        'comments',
+        'user_id',  // añadir este campo para saber quién hizo el comentario
+        'status'    // añadir este campo para registrar el estado en ese momento
     ];
 
     public function derivation(): BelongsTo
     {
         return $this->belongsTo(Derivation::class);
+    }
+    
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
     }
 }
