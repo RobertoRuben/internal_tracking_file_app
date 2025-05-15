67d527d4aa3f1d9bd876eb6af81d0ee1f8561599
RobertoRuben <robertoch263@gmail.com>
2025-05-15 11:46:31 -0500

    ✨ feat(middleware): Add EnsureUserIsActive middleware to enforce active user status

--- app/Providers/Filament/AppPanelProvider.php
+++ app/Providers/Filament/AppPanelProvider.php
@@ -53,6 +53,7 @@ class AppPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
+                \App\Http\Middleware\EnsureUserIsActive::class,
             ])
             ->databaseNotifications();
     }
